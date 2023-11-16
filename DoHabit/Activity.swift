25488b5a2d6a054efa93f8297c95a15e2d51075a
ygorsimoura <ygorsimoura01@gmail.com>
2023-11-16 11:41:06 -0300

    Saving Habits data with UserDefaults and add a dismiss method in save button

--- DoHabit/Activity.swift
+++ DoHabit/Activity.swift
@@ -9,12 +9,29 @@ import Foundation
 import SwiftUI
 
 struct Activity: Codable, Identifiable {
-    let id: UUID
+    var id: UUID
     let title: String
     let description: String
     var days: Int
 }
 
 class Activities: ObservableObject {
-    @Published var items = [Activity]()
+    @Published var items = [Activity]() {
+        didSet {
+            if let encoded = try? JSONEncoder().encode(items){
+                UserDefaults.standard.set(encoded, forKey: "Items")
+            }
+        }
+    }
+    
+    init() {
+        if let savedItems = UserDefaults.standard.data(forKey: "Items"){
+            if let decodedItems = try? JSONDecoder().decode([Activity].self , from: savedItems) {
+                items = decodedItems
+                return
+            }
+        }
+        
+        items = [] // caso falhem será um array vazio
+    }
 }
